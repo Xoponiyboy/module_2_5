d3c9c981ae4e686404b985db1e94084c2b1edbee
Xoponiyboy <ilyailyanov@yandex.ru>
2024-06-01 12:19:07 +0300

    Add files via upload

---
+++ module_2_5_.py
@@ -0,0 +1,8 @@
+def get_matrix(n, m, value):
+    return [[value] * m] * n
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
